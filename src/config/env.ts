495b55157b737e7267718f5539cc4e51f869e555
esau-morais <esaumorais7@gmail.com>
2024-11-26 21:23:01 -0300

    chore: init project

---
+++ src/config/env.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+import Joi from 'joi'
+import path from 'path'
+
+export const loadConfig = () => {
+  const envPath = path.join(__dirname, '..', '..', '.env')
+
+  const result = dotenv.config({ path: envPath })
+  if (result.error) {
+    throw new Error(
+      `Failed to load .env file from path ${envPath}: ${result.error.message}`,
+    )
+  }
+
+  const schema = Joi.object({
+    API_HOST: Joi.string().required(),
+    API_PORT: Joi.string().required(),
+    LOG_LEVEL: Joi.string()
+      .valid('debug', 'info', 'warn', 'error', 'fatal')
+      .required(),
+  }).unknown(true)
+
+  const { error } = schema.validate(process.env, { abortEarly: false })
+  if (error) {
+    throw new Error(`Config validation error: ${error.message}`)
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import fastify from "fastify";
+import { loadConfig } from "./config/env";
+
+loadConfig();
+
+const host = String(process.env.API_HOST);
+const port = Number(process.env.API_PORT);
+
+const init = async () => {
+  const server = fastify({
+    logger: true,
+  });
+
+  const signals: NodeJS.Signals[] = ["SIGINT", "SIGTERM"];
+  signals.forEach((signal) => {
+    process.on(signal, async () => {
+      try {
+        await server.close();
+        server.log.error(`Closed application on ${signal}`);
+        process.exit(0);
+      } catch (err) {
+        server.log.error(`Error closing application on ${signal}`, err);
+        process.exit(1);
+      }
+    });
+  });
+
+  try {
+    await server.listen({ host, port });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+process.on("unhandledRejection", (err) => {
+  console.error("unhandledRejection: ", err);
+  process.exit(1);
+});
+
+init();
